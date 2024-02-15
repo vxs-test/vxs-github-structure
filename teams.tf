0eb7acf66ca62042480c4d540ef3b66edaf8bb03
Stephane Bruckert <stephane.bruckert@gmail.com>
2024-02-15 00:28:55 +0000

    Create teams

--- teams.tf
+++ teams.tf
@@ -1,11 +1,11 @@
-#resource "github_team" "backend" {
-#  name        = "backend"
-#  description = "The backend team"
-#  privacy     = "closed"
-#}
-#
-#resource "github_team" "frontend" {
-#  name        = "frontend"
-#  description = "The frontend team"
-#  privacy     = "closed"
-#}
+resource "github_team" "backend" {
+  name        = "backend"
+  description = "The backend team"
+  privacy     = "closed"
+}
+
+resource "github_team" "frontend" {
+  name        = "frontend"
+  description = "The frontend team"
+  privacy     = "closed"
+}
